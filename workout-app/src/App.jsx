fbbce9013ef1d169208ec10b1f8349cc9b094973
TroySandy <troysandy16@gmail.com>
2021-04-14 12:49:41 -0400

    First push

---
+++ workout-app/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import Sitebar from "./home/Navbar";
+import Auth from "./auth/Auth.jsx";
+
+function App() {
+  const [sessionToken, setSessionToken] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+    console.log('done');
+  }
+
+  return (
+    <div>
+      <Sitebar clickLogout={clearToken} />
+      <Auth updateToken={updateToken} />
+    </div>
+  );
+}
+
+export default App;
---
+++ workout-app/src/auth/Signup.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+const Signup = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:4000/user/register", {
+      method: "POST",
+      body: JSON.stringify({   //this is where you make the route to the backend, must match it
+          username: username, passwordhash: password ,
+      }),
+      headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        props.updateToken(data.sessionToken);
+        console.log(data.sessionToken);
+    });
+  };    
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="username">Username</Label>
+          <Input
+            onChange={(e) => setUsername(e.target.value)}
+            name="username"
+            value={username}
+            required
+            
+          />
+          {(username === '') ? (<div><p>Username Required</p></div>) : null }
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="passwordhash">Password</Label>
+          <Input
+            onChange={(e) => setPassword(e.target.value)}
+            name="passwordhash"
+            value={password}
+            
+          />
+        </FormGroup>
+        <Button type="submit">Sign Up</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Signup;
+
+
+// const ValidateSignUp = (username) => {
+//     if(username) ? <div></div> : <div>Username required</div>
+// }
+
+// {!result || !result.poster_path ? null : (
+//     <MovieAppDisplay movie={result} />
+//   )}
